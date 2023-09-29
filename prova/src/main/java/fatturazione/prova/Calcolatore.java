e762d13aa08fc84b72f76ecaa2544406c6d9b9aa
4TK4 <145766850+4TK4@users.noreply.github.com>
2023-09-29 12:53:15 +0200

    jhsggjd
    ho aggiunto il progetto maven

---
+++ prova/src/main/java/fatturazione/prova/Calcolatore.java
@@ -0,0 +1,14 @@
+package fatturazione.prova;
+
+import org.apache. logging. log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Calcolatore {
+	
+	static Logger LOG = LogManager. getLogger();
+	
+	public static void main (String[] args) {
+		LOG. error ("sto usando il Calcolatore");
+	}
+
+}
